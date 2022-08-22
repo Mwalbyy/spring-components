ecdf233cac4f7f416133826e5a6bbd8df91ed92d
Marcus Walby <marcuswalby24@gmail.com>
2022-08-22 16:45:00 -0400

    framer motion implimented and carousel is working

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,5 +1,5 @@
 import { React, useState } from "react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Carousel() {
   let [count, setCount] = useState(1);
@@ -9,19 +9,25 @@ export default function Carousel() {
     <>
       <div className="carousel">
         <button onClick={() => setCount(count - 1)} className="carousel__btn">
-        <i class="fa-solid fa-chevron-left"></i>
+          <i class="fa-solid fa-chevron-left"></i>
         </button>
         <button onClick={() => setCount(count + 1)} className="carousel__btn">
-        <i class="fa-solid fa-chevron-right"></i>
+          <i class="fa-solid fa-chevron-right"></i>
         </button>
       </div>
       <div className="carousel__container">
         <div className="carousel__div">
-          <motion.div 
-          key={count}
-          initial={{ x: 100 }}
-          animate={{ x: 0 }}
-          className={`carousel__div__count ${colors[Math.abs(count) % 4]}`}>{count}</motion.div>
+          <AnimatePresence>
+            <motion.div
+              key={count}
+              initial={{ x: 100 }}
+              animate={{ x: 0 }}
+              exit={{ x: -100 }}
+              className={`carousel__div__count ${colors[Math.abs(count) % 4]}`}
+            >
+              {count}
+            </motion.div>
+          </AnimatePresence>
         </div>
       </div>
     </>
@@ -31,3 +37,4 @@ export default function Carousel() {
 let colors = ["red", "green", "blue", "yellow"];
 
 // https://www.youtube.com/watch?v=aV2YJuxQ2vo&ab_channel=SamSelikoff
+// 8:00
