19c32a482051376d5cda34a2fbe416aa304d5005
Marcus Walby <marcuswalby24@gmail.com>
2022-08-30 15:50:00 -0400

    commented on the carousel code

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -2,7 +2,9 @@ import { React, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
 export default function Carousel() {
+  // count is used to keep track of the current slide
   let [count, setCount] = useState(1);
+  // tuple is used to store the direction of the carousel 
   let [tuple, setTuple] = useState([null, count]);
 
   if (tuple[1] !== count) {
@@ -11,7 +13,7 @@ export default function Carousel() {
   console.log(tuple);
 
   let prev = tuple[0];
-
+  // using the tuple state to determine the direction of the carousel
   let direction = count > prev ? 1 : -1;
 
   return (
@@ -44,7 +46,7 @@ export default function Carousel() {
     </>
   );
 }
-
+// variants allows for the animation of the carousel to be controlled by direction variable
 let variants = {
   enter: (direction) => ({ x: direction *100 }),
   center: { x: 0 },
