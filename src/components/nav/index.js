982ba9d2153dbe67796483d2ad36507ab81e3caa
Marcus Walby <marcuswalby24@gmail.com>
2022-09-02 19:52:55 -0400

    min set

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,40 +1,14 @@
 import React from "react";
-import { motion, useScroll } from "framer-motion";
+import { motion } from "framer-motion";
+import Image from "../images/laptop.jpg"
 
 export default function Navbar() {
-  const { scrollYProgress } = useScroll()
 
   return (
     <>
-      <motion.nav className="navbar" style={{opacity: 0}}>
-        <p>marcus walby</p>
-        <ul className="navbar__list" >
-          <motion.li
-            whileHover={{
-              scale: 1.2,
-              transition: { duration: 0.2 },
-            }}
-          >
-            Projects
-          </motion.li>
-          <motion.li
-            whileHover={{
-              scale: 1.2,
-              transition: { duration: 0.2 },
-            }}
-          >
-            About
-          </motion.li>
-          <motion.li
-            whileHover={{
-              scale: 1.2,
-              transition: { duration: 0.2 },
-            }}
-          >
-            Contact
-          </motion.li>
-        </ul>
-      </motion.nav>
+      <div className="imgContainer">
+        <img src={Image} alt="laptop" />
+      </div>
     </>
   );
 }
