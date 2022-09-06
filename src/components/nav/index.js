8312f9eb9abc3812a6ded4adc55a3962f6721f87
Marcus Walby <marcuswalby24@gmail.com>
2022-09-06 19:28:16 -0400

    motion

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { motion } from "framer-motion";
-import Image from "../images/laptop.jpg"
+import Image from "../images/laptop.jpg";
 
 export default function Navbar() {
-
   return (
     <>
-      <div className="imgContainer">
-        <img src={Image} alt="laptop" />
+      <div className="home">
+        <div className="home__header">
+          <p>Hi, I'm Marcus Walby</p>
+        </div>
+
+        <div className="home__img">
+          {/* <motion.img whileHover={{ scale: 1.2 }} src={Image} alt="laptop" /> */}
+        </div>
       </div>
     </>
   );
