7c9331224c244d7bffe8e25d60c341a56a9ea738
Marcus Walby <marcuswalby24@gmail.com>
2022-09-09 00:06:07 -0400

    framer motion mapping

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { motion } from "framer-motion";
-import Image from "../images/laptop.jpg";
+// import Image from "../images/laptop.jpg";
 
 export default function Navbar() {
+  const header = ["H","e","l","l","o"]
   return (
     <>
       <div className="home">
-        <div className="home__header">
-          <p>Hi, I'm Marcus Walby</p>
-        </div>
+        <span className="home__header">
+          {header.map((V, i) => <motion.p key={i} whileHover={{scale: 1.2}}>{V}</motion.p>
+          )}
+        </span>
 
         <div className="home__img">
           {/* <motion.img whileHover={{ scale: 1.2 }} src={Image} alt="laptop" /> */}
